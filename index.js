1b4de5dc27dde33a9534506b232728af0a24887e
Jacqueline <quackenbushjacqueline@gmail.com>
2022-09-26 10:11:46 -0400

    beginning of menu

---
+++ index.js
@@ -0,0 +1,20 @@
+
+// nav-links-container have class hidden and display: None 
+// close-icon has display: None 
+// menu-icon onClick  triggers function and adds class active to nav-links 
+//     and they have display flex column
+
+//     while nav-links has class active:
+//     menu-icon has display: none 
+//     close-icon has display: inline-block(?) 
+
+//     close-icon onClick triggers function and nav-links
+//     loses class active 
+
+function toggleMenu() {
+    let navLinks = document.getElementById('nav-links-container');
+    // let closeIcon= document.getElementById('close-icon');
+    navLinks.classList.toggle('active')
+    // closeIcon.classList.toggle('active')
+ 
+}
